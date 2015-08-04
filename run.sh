8a3f2433cae163b27fbebb64c90302661f5a6375
Christoph Görn <goern@redhat.com>
2015-08-04 23:13:28 +0200

    removed the crap added an index

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p $HOME/.rhc/git $HOME/.rhc/openshift $HOME/.rhc/ssh $HOME/workspace/rhc
-
-docker run --rm -ti -p 3000 -v $HOME/.rhc/git:/root/.git -v $HOME/workspace/rhc:/root/src -v /home/goern/.rhc/openshift:/root/.openshift -v /home/goern/.rhc/ssh:/root/.ssh rhc /bin/bash
-
-#end.
